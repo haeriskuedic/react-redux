1171ae0c3d29f13d560c8fe41f24f644a54a5a61
haeriskuedic <haris.kudic@hotmail.de>
2023-06-21 21:12:33 +0200

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,42 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch(); // a function that will dispatch a action against our redux store
+
+  // redux will automatic create a subscription for this component
+  const counter = useSelector((state) => state.counter); // fuction retrieve the store data and extract
+  const showCounter = useSelector((state) => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 }); // dispatch with action payload (Custom data)
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
